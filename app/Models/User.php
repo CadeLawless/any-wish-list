457b8a9f988814055f6a64d903f386a3392e169d
Cade Lawless <clawless05@gmail.com>
2026-07-11 10:45:24 -0700

    cleaned up previously tracked images and added expiration check back to reset password

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,7 +46,7 @@ class User extends Model
     {
         //var_dump("SELECT * FROM " . static::$table . " WHERE reset_password_key = ? AND reset_password_expiration > NOW()", $key);
         //exit;
-        $stmt = \App\Core\Database::query("SELECT * FROM " . static::$table . " WHERE reset_password_key = ?", [$key]);
+        $stmt = \App\Core\Database::query("SELECT * FROM " . static::$table . " WHERE reset_password_key = ? AND reset_password_expiration > NOW()", [$key]);
         $result = $stmt->get_result()->fetch_assoc();
         return $result ?: null;
     }
